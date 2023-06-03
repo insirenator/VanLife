910649278996177ec71f4d19b998394c4233b24c
insirenator <arsshakeeb149@protonmail.com>
2023-06-03 14:56:10 +0530

    Added gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   const config = {
     plugins: [react()],
-    base: "/",
+    base: "/VanLife",
   };
 
-  if (command !== "serve") {
-    config.base = "/VanLife/";
-  }
+  // if (command !== "serve") {
+  //   config.base = "/VanLife/";
+  // }
 
   return config;
 });
