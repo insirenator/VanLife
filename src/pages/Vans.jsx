0d8a7e8eae01b5ddc5bddf704c9a8ebd8acce438
insirenator <arsshakeeb149@protonmail.com>
2023-06-05 16:50:56 +0530

    Added: type filters

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 function colorByType(type) {
   switch (type) {
@@ -57,7 +57,11 @@ function Van({ id, name, type, price, imageUrl }) {
 
 export default function Vans() {
   const [vans, setVans] = useState([]);
-  console.log(vans);
+  const [filters, setFilters] = useState([]);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const typeFilter = searchParams.getAll("type");
+  console.log(typeFilter);
 
   useEffect(() => {
     fetch("/api/vans")
@@ -66,11 +70,33 @@ export default function Vans() {
       .catch((err) => console.log(err));
   }, []);
 
+  function handleFilter(e, type) {
+    if (filters.includes(type)) {
+      e.target.style.borderColor = "white";
+      e.target.style.color = "white";
+      setFilters(filters.filter((t) => t !== type));
+    } else {
+      e.target.style.borderColor = "rgb(255, 155, 74)";
+      e.target.style.color = "rgb(255, 155, 74)";
+      setFilters([...filters, type]);
+    }
+  }
+
+  const displayedVans =
+    filters.length > 0
+      ? vans.filter((van) => filters.includes(van.type))
+      : vans;
+
   return (
     <>
       <h1 className="van-page-title">Explore all our Options!</h1>
+      <div className="van-page-filters">
+        <p onClick={(e) => handleFilter(e, "simple")}>Simple</p>
+        <p onClick={(e) => handleFilter(e, "rugged")}>Rugged</p>
+        <p onClick={(e) => handleFilter(e, "luxury")}>Luxury</p>
+      </div>
       <div className="van-list">
-        {vans.map((van, idx) => (
+        {displayedVans.map((van, idx) => (
           <Van
             key={idx}
             id={van.id}
